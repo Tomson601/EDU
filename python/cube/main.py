b2d7cdd2947d2f450e69d55f418fd14447a401c1
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-05-15 15:14:10 +0200

    Sinus complete

--- python/cube/main.py
+++ python/cube/main.py
@@ -2,24 +2,40 @@ import pygame
 import math
 
 pygame.init()
+pygame.display.set_caption("3D Cube")
 
-WIDTH=360
-HEIGHT=360
+WIDTH=1200
+HEIGHT=720
 
 screen=pygame.display.set_mode((WIDTH, HEIGHT))
 
 white=(255, 255, 255)
 black=(0, 0, 0)
+green=(0,255,0)
 
 x_offset=WIDTH/2
 y_offset=HEIGHT/2
 
 
 def draw(x, y):
-    pygame.draw.circle(screen, white, (x+x_offset, y+y_offset), 1)
+    pygame.draw.circle(screen, green, (x+x_offset, y+y_offset), 1)
+
+x = -720
+rads = 0
 
 while True:
-    for i in range(1000):
-        draw(0, i)
+    for rads in range(360):
         pygame.display.update()
+        draw(x, 10*math.sin(math.radians(rads)))
+        rads+=5
+        draw(x, 15*math.sin(math.radians(rads)))
+        rads+=10
+        draw(x, 20*math.sin(math.radians(rads)))
+        x+=0.5
+        rads+=15
 
+    #pygame.display.update()
+    for event in pygame.event.get():    # Quit loop
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
