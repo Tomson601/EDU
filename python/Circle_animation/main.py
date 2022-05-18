3678377dca87973fce3c8ad05ea3d4d1136318a9
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-05-18 15:50:46 +0200

    Changing dif lvl

--- python/Circle_animation/main.py
+++ python/Circle_animation/main.py
@@ -1,8 +1,11 @@
+import time
 import pygame
 import math
+import random
+
 
 pygame.init()
-pygame.display.set_caption("Rotating Polygon")
+pygame.display.set_caption("Circle animation")
 
 WIDTH=1200
 HEIGHT=720
@@ -15,29 +18,37 @@ y_offset=HEIGHT/2
 black = (0, 0, 0)
 green=(0, 255, 0)
 
+r=(random.randint(0, 255))
+g=(random.randint(0, 255))
+b=(random.randint(0, 255))
 
-def draw(x, y):
-    pygame.draw.circle(screen, green, (x+x_offset, y+y_offset), 1)
+colours = (r, g, b)
 
 
 def draw_polygon(x1, y1, x2,y2, x3, y3):
     pygame.draw.polygon(screen, green, [(x1,y1), (x2,y2), (x3,y3)], 2)
 starting_pos = (450, 446, 600, 186, 750, 446)
 
-x1=450
-y1=446
-x2=600
-y2=186
-x3=750
-y3=446
-
 prog_control=True
-
+step = 1
 while True:
-    #draw_polygon(450, 446, 600, 186, 750, 446)      #UP
-    #draw_polygon(686, 510, 426, 360, 686, 210)      #LEFT
-    #TODO: Step by step rotation (by 1 pixel)
-    for event in pygame.event.get():                                    # Quit loop
+    pygame.draw.circle(screen, colours, (600, 360), step)
+    pygame.display.update()
+
+    r = (random.randint(0, 255))
+    g = (random.randint(0, 255))
+    b = (random.randint(0, 255))
+    
+    print(r,g,b)
+    time.sleep(0.05)
+    screen.fill(black)
+
+    step += 1
+
+    if step == 200:
+        i = 0
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
