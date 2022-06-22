4aeb2512d49382d5c83c4b6f836cf62ddeb4edce
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-06-22 21:16:00 +0200

    Updating arduino project

--- arduino/external_eeprom/external_eeprom.ino
+++ arduino/external_eeprom/external_eeprom.ino
@@ -7,14 +7,12 @@ int max_address = 127;
 
 
 byte readEEPROM(int address, int i2c_address){
-  byte rcvData = 0x70;
+  byte rcvData = 0xFF;
   Wire.beginTransmission(i2c_address);
  
   // Send memory address as two 8-bit bytes
   Wire.write((int)(address >> 0));   // MSB
-  Wire.write((int)(address & 0x70)); // LSB
- 
-  // End the transmission
+  Wire.write((int)(address & 0xFF)); // LSB
   Wire.endTransmission();
  
   // Request one byte of data at current memory address
@@ -22,27 +20,25 @@ byte readEEPROM(int address, int i2c_address){
  
   // Read the data and assign to variable
   rcvData =  Wire.read();
- 
-  // Return the data as function output
   return rcvData;
 }
+
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   delay(1000);
  
-  for (int address = 0; address <= max_address; address++) {
+  for (int address = -1; address <= max_address - 1; address++) {
     readValue = readEEPROM(address, EEPROM_I2C_ADDRESS);
-
     Serial.print("ADDR = ");
-    Serial.print(address);
+    Serial.print(address+1);
     Serial.print("\t");
-    Serial.println(readValue);
- 
+    // Serial.println(readValue, HEX);
+    Serial.write(readValue);
+    Serial.print("\n");
   }
 }
 
-
 void loop() {
-
+  delay(1000);
 }
