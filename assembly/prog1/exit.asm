418e13b97975c32969223d68bea975767ea10c1b
Tomasz Jarko <tomasz.jarko@gmail.com>
2022-08-05 22:12:44 +0200

    Adding some assebmly language

---
+++ assembly/prog1/exit.asm
@@ -0,0 +1,34 @@
+# PURPOSE: Simple program that exits and returns a 
+#          status code back to the Linux kernel
+#
+#
+
+# INPUT: none
+#
+#
+
+# OUTPUT: returns a status code. This can be viewd by 
+#         executing "echo $?" command after runnig the 
+#         program
+#
+#
+
+# VARIABLES:
+#	%eax holds the system call number
+#	%ebx holds the system return status
+#
+
+.section .data
+
+.section .text
+.globl _start
+
+_start:
+movl $1, %eax    # this is the linux kernel command number
+                # (system call) for exiting a program
+
+movl $0, %ebx    # this is the status number which will be
+                # return to the operating system.
+
+int $0x80       # this wakes up the kernel to run the
+                # exit command
